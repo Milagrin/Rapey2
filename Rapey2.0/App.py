25c1f3b8928a1401b7222e199e4772d73c36807c
Milagrin <40966764+Milagrin@users.noreply.github.com>
2020-11-13 17:03:30 -0300

    Se agrega repartidor crud

--- Rapey2.0/App.py
+++ Rapey2.0/App.py
@@ -24,13 +24,13 @@ def index_cliente():
 @app.route('/agregar_cliente', methods=['POST'])
 def agregar_cliente():
     if request.method == 'POST':
-        rut = request.form['Rut']
         nombre = request.form['Nombre']
+        rut = request.form['Rut']
         edad = request.form['Edad']
         direccion = request.form['Direccion']
         cursor = mysql.connection.cursor()
-        cursor.execute('INSERT INTO cliente (rut_cliente, nombre_completo, edad, direccion) VALUES (%s,%s,%s,%s)',
-        (rut, nombre, edad, direccion))
+        cursor.execute('INSERT INTO cliente (Nombre_cliente,Rut_cliente, Edad, Direccion) VALUES (%s,%s,%s,%s)',
+        (nombre,rut, edad, direccion))
         mysql.connection.commit()
         flash('Cliente agregado correctamente')
         return redirect(url_for('index_cliente'))
@@ -38,26 +38,26 @@ def agregar_cliente():
 @app.route('/edit_cliente/<string:rut>')
 def get_cliente(rut):
     cursor = mysql.connection.cursor()
-    cursor.execute('SELECT * FROM cliente WHERE rut_cliente = %s', (rut))
+    cursor.execute('SELECT * FROM cliente WHERE Rut_cliente = %s', (rut))
     dato = cursor.fetchall()
     return render_template('edit-clientes.html', clientes = dato[0])
 
 @app.route('/update/<string:rut>', methods = ['POST'])
 def update_cliente(rut):
     if request.method == 'POST':
-        rut1 = request.form['Rut']
         nombre = request.form['Nombre']
+        rut1 = request.form['Rut']
         edad = request.form['Edad']
         direccion = request.form['Direccion']
         cursor = mysql.connection.cursor()
         cursor.execute("""
             UPDATE cliente
-            SET rut_cliente = %s,
-                nombre_completo = %s,
-                edad = %s,
-                direccion = %s
+            SET Nombre_cliente = %s,
+                Rut_cliente = %s,
+                Edad = %s,
+                Direccion = %s
             WHERE rut_cliente = %s
-        """,(rut1,nombre, edad,direccion, rut))
+        """,(nombre,rut1, edad,direccion, rut))
         mysql.connection.commit()
         flash('Cliente actualizado correctamente')
         return redirect(url_for('index_cliente'))
@@ -65,7 +65,7 @@ def update_cliente(rut):
 @app.route('/delete_cliente/<string:rut>')
 def delete_cliente(rut):
     cursor = mysql.connection.cursor()
-    cursor.execute('DELETE FROM cliente WHERE rut_cliente = {0}'.format(rut))
+    cursor.execute('DELETE FROM cliente WHERE Rut_cliente = {0}'.format(rut))
     mysql.connection.commit()
     flash('Cliente eliminado correctamente')
     return redirect(url_for('index_cliente'))
--- Rapey2.0/App2.py
+++ Rapey2.0/App2.py
@@ -2,10 +2,37 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+# Mysql Connection
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'rapey'
+mysql = MySQL(app)
 
+#Configuraciones
+app.secret_key = 'mysecretkey'
+
+#CRUD Repartidores
 @app.route('/')
 def index_Repartidor():
-    return render_template('sesion-repartidor.html')
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM repartidor')
+    datos = cursor.fetchall
+    return render_template('sesion-repartidor.html', repartidor = datos)
+
+@app.route('/agregar_repartidor', methods = ['POST'])
+def agregar_repartidor():
+    if request.method == 'POST':
+        nombre = request.form['Nombre']
+        rut = request.form['Rut']
+        region = request.form['Region']
+        telefono = request.form['Telefono']
+        cursor = mysql.connection.cursor()
+        cursor.execute('INSERT INTO repartidor (Nombre_repartidor, Rut_repartidor, Nro_region, telefono) VALUES (%s, %s, %s, %s)',
+        (nombre,rut,region,telefono))
+        mysql.connection.commit()
+        flash('Repartidor agregado correctamente')
+        return redirect(url_for('index_Repartidor'))
 
 if __name__ == '__main__':
     app.run(port=3001,debug=True)
